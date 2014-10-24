aaae7b4a42b1dbb67e4239434d32658543f21fa9
silencious <tjh0473@gmail.com>
2014-10-24 08:35:55 +0800

    backup

---
+++ lab1/bits.c
@@ -0,0 +1,352 @@
+/* 
+ * CS:APP Data Lab 
+ * 
+ * bits.c - Source file with your solutions to the Lab.
+ *          This is the file you will hand in to your instructor.
+ *
+ * WARNING: Do not include the <stdio.h> header; it confuses the dlc
+ * compiler. You can still use printf for debugging without including
+ * <stdio.h>, although you might get a compiler warning. In general,
+ * it's not good practice to ignore compiler warnings, but in this
+ * case it's OK.  
+ */
+
+#include "btest.h"
+#include <limits.h>
+
+/*
+ * Instructions to Students:
+ *
+ * STEP 1: Fill in the following struct with your identifying info.
+ */
+team_struct team =
+{
+   /* Team name: Replace with either:
+      Your login ID if working as a one person team
+      or, ID1+ID2 where ID1 is the login ID of the first team member
+      and ID2 is the login ID of the second team member */
+    "5130379056", 
+   /* Student name 1: Replace with the full name of first team member */
+   "Tian JiaHe",
+   /* Login ID 1: Replace with the login ID of first team member */
+   "5130379056",
+
+   /* The following should only be changed if there are two team members */
+   /* Student name 2: Full name of the second team member */
+   "",
+   /* Login ID 2: Login ID of the second team member */
+   ""
+};
+
+#if 0
+/*
+ * STEP 2: Read the following instructions carefully.
+ */
+
+You will provide your solution to the Data Lab by
+editing the collection of functions in this source file.
+
+CODING RULES:
+ 
+  Replace the "return" statement in each function with one
+  or more lines of C code that implements the function. Your code 
+  must conform to the following style:
+ 
+  int Funct(arg1, arg2, ...) {
+      /* brief description of how your implementation works */
+      int var1 = Expr1;
+      ...
+      int varM = ExprM;
+
+      varJ = ExprJ;
+      ...
+      varN = ExprN;
+      return ExprR;
+  }
+
+  Each "Expr" is an expression using ONLY the following:
+  1. Integer constants 0 through 255 (0xFF), inclusive. You are
+      not allowed to use big constants such as 0xffffffff.
+  2. Function arguments and local variables (no global variables).
+  3. Unary integer operations ! ~
+  4. Binary integer operations & ^ | + << >>
+    
+  Some of the problems restrict the set of allowed operators even further.
+  Each "Expr" may consist of multiple operators. You are not restricted to
+  one operator per line.
+
+  You are expressly forbidden to:
+  1. Use any control constructs such as if, do, while, for, switch, etc.
+  2. Define or use any macros.
+  3. Define any additional functions in this file.
+  4. Call any functions.
+  5. Use any other operations, such as &&, ||, -, or ?:
+  6. Use any form of casting.
+ 
+  You may assume that your machine:
+  1. Uses 2s complement, 32-bit representations of integers.
+  2. Performs right shifts arithmetically.
+  3. Has unpredictable behavior when shifting an integer by more
+     than the word size.
+
+EXAMPLES OF ACCEPTABLE CODING STYLE:
+  /*
+   * pow2plus1 - returns 2^x + 1, where 0 <= x <= 31
+   */
+  int pow2plus1(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     return (1 << x) + 1;
+  }
+
+  /*
+   * pow2plus4 - returns 2^x + 4, where 0 <= x <= 31
+   */
+  int pow2plus4(int x) {
+     /* exploit ability of shifts to compute powers of 2 */
+     int result = (1 << x);
+     result += 4;
+     return result;
+  }
+
+
+NOTES:
+  1. Use the dlc (data lab checker) compiler (described in the handout) to 
+     check the legality of your solutions.
+  2. Each function has a maximum number of operators (! ~ & ^ | + << >>)
+     that you are allowed to use for your implementation of the function. 
+     The max operator count is checked by dlc. Note that '=' is not 
+     counted; you may use as many of these as you want without penalty.
+  3. Use the btest test harness to check your functions for correctness.
+  4. The maximum number of ops for each function is given in the
+     header comment for each function. If there are any inconsistencies 
+     between the maximum ops in the writeup and in this file, consider
+     this file the authoritative source.
+#endif
+
+/*
+ * STEP 3: Modify the following functions according the coding rules.
+ * 
+ *   IMPORTANT. TO AVOID GRADING SURPRISES:
+ *   1. Use the dlc compiler to check that your solutions conform
+ *      to the coding rules.
+ *   2. Use the btest test harness to check that your solutions produce 
+ *      the correct answers. Watch out for corner cases around Tmin and Tmax.
+ */
+/* 
+ * abs - absolute value of x (except returns TMin for TMin)
+ *   Example: abs(-1) = 1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 10
+ *   Rating: 4
+ */
+int abs(int x) {
+  /* use shift on x to get 0 or Tmin depending on the sign bit,
+ thus get the original number or the opposite number accordingly*/
+  int m=x>>31;
+  return (x^m)+(m&1);
+}
+/* 
+ * bitAnd - x&y using only ~ and | 
+ *   Example: bitAnd(6, 5) = 4
+ *   Legal ops: ~ |
+ *   Max ops: 8
+ *   Rating: 1
+ */
+int bitAnd(int x, int y) {
+  /* I think this one needs no explanation... */
+
+  return ~((~x)|(~y));
+
+}
+/* 
+ * bitMask - Generate a mask consisting of all 1's 
+ *   lowbit and highbit
+ *   Examples: bitMask(5,3) = 0x38
+ *   Assume 0 <= lowbit <= 31, and 0 <= highbit <= 31
+ *   If lowbit > highbit, then mask should be all 0's
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+int bitMask(int highbit, int lowbit) {
+  /* use shift to get bits over lowbit and highbit set 1*/
+  int m1=(~0)<<lowbit;
+  int m2=(~0)<<highbit<<1;
+  return (m1^m2)&m1;
+}
+/* 
+ * bitXor - x^y using only ~ and & 
+ *   Example: bitXor(4, 5) = 1
+ *   Legal ops: ~ &
+ *   Max ops: 14
+ *   Rating: 2
+ */
+int bitXor(int x, int y) {
+  /* pick the bits when x and y are the same, combine their opposite*/
+  return ( ~(x & y) ) & ~( (~x) & (~y) );
+}
+/* 
+ * conditional - same as x ? y : z 
+ *   Example: conditional(2,4,5) = 4
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+int conditional(int x, int y, int z) {
+  /* if x is true, then !x=0, !!x=1, else x=0, !x=1. get Tmin from 1. */
+  int a=((!x)<<31)>>31;
+  int b=((!!x)<<31)>>31;
+  return (b & y) | (a & z);
+}
+/* 
+ * evenBits - return word with all even-numbered bits set to 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 8
+ *   Rating: 2
+ */
+int evenBits(void) {
+  /* use shift to get higher bits set to 1 */
+  int m1=0x55;
+  m1=m1+(m1<<8);
+  return m1+(m1<<16);
+}
+/* 
+ * isEqual - return 1 if x == y, and 0 otherwise 
+ *   Examples: isEqual(5,5) = 1, isEqual(4,5) = 0
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 5
+ *   Rating: 2
+ */
+int isEqual(int x, int y) {
+  /* x^y=0 if and only if x==y */
+  return !(x^y);
+}
+/* 
+ * isLess - if x < y  then return 1, else return 0 
+ *   Example: isLess(4,5) = 1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 24
+ *   Rating: 3
+ */
+int isLess(int x, int y) {
+  /* first exclude the overflow situation, then use (x-y)'s sign bit to represent other cases */
+  int xpyn=(!(x>>31))&!!(y>>31);	//x>0&&y<0
+  int xnyp=(!!(x>>31))&!(y>>31);	//x<0&&y>0
+  int xly=!!((x+~y+1)>>31);		//x-y<0
+  return (!xpyn)&(xnyp|xly);
+}
+/* 
+ * isNegative - return 1 if x < 0, return 0 otherwise 
+ *   Example: isNegative(-1) = 1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 3
+ */
+int isNegative(int x) {
+  /* use shift to get sign-bit mask */
+  int m=(1<<31);
+  return !!(x&m);
+}
+/* 
+ * isNonZero - Check whether x is nonzero using
+ *              the legal operators except !
+ *   Examples: isNonZero(3) = 1, isNonZero(0) = 0
+ *   Legal ops: ~ & ^ | + << >>
+ *   Max ops: 10
+ *   Rating: 4 
+ */
+int isNonZero(int x) {
+  /* x and x's opposite are both 0 only if x==0*/ 
+  return ((x|(~x+1))>>31)&1;
+}
+/*
+ * isPower2 - returns 1 if x is a power of 2, and 0 otherwise
+ *   Examples: isPower2(5) = 0, isPower2(8) = 1, isPower2(0) = 0
+ *   Note that no negative number is a power of 2.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 60
+ *   Rating: 4
+ */
+int isPower2(int x) {
+  /* add all 1 bits to cnt and subtract 1 to see if there's no other factor than 2 */
+  int m1=0x11;
+  int m2=m1+(m1<<8);
+  int m3=m2+(m2<<16);
+  int cnt=x&m3;
+  cnt=cnt+((x>>1)&m3);
+  cnt=cnt+((x>>2)&m3);
+  cnt=cnt+((x>>3)&m3);
+  cnt=cnt+(cnt>>16);
+  m1=m1+(m1<<8);
+  cnt=(cnt&m1)+((cnt>>4)&m1);
+  cnt=cnt+(cnt>>8);
+  return !(x>>31) & !(cnt+~0);
+}
+/* 
+ * leastBitPos - return a mask that marks the position of the
+ *               least significant 1 bit. If x == 0, return 0
+ *   Example: leastBitPos(96) = 0x20
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 4 
+ */
+int leastBitPos(int x) {
+  /* x's zero bits after the least significant 1 bit are transformed to 0 in ~x,
+ thus ~x+1 and x have only one same bit that are both 1, and it's the least significant bit */
+  return x&(~x+1);
+}
+/* 
+ * logicalNeg - implement the ! operator, using all of 
+ *              the legal operators except !
+ *   Examples: logicalNeg(3) = 0, logicalNeg(0) = 1
+ *   Legal ops: ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 4 
+ */
+int logicalNeg(int x) {
+  /* only 0's opposite is itself, others' sign bit are different from their opposites';
+ Tmin+1=0 */
+  return ((x|(~x+1))>>31)+1;
+}
+/* 
+ * reverseBytes - reverse the bytes of x
+ *   Example: reverseBytes(0x01020304) = 0x04030201
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 25
+ *   Rating: 3
+ */
+int reverseBytes(int x) {
+  /* use shift to get mask which helps keep bytes temporarily */
+  int m1=0xFF;
+  int m2 = m1 + (m1 << 8);
+  int result = ((x & m2) << 16) + ((x >> 16) & m2);
+  int m3 = m1 + (m1 << 16);
+  result = ((result & m3) << 8) + ((result >> 8) & m3);
+  return result;
+}
+/* 
+ * sum3 - x+y+z using only a single '+'
+ *   Example: sum3(3, 4, 5) = 12
+ *   Legal ops: ! ~ & ^ | << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+/* A helper routine to perform the addition.  Don't change this code */
+static int sum(int x, int y) {
+  return x+y;
+}
+int sum3(int x, int y, int z) {
+  int word1 = 0;
+  int word2 = 0;
+  /**************************************************************
+     Fill in code below that computes values for word1 and word2
+     without using any '+' operations 
+  ***************************************************************/
+  /*use word1 to keep the original bits, use word2 to keep carry*/
+  word1=(x^y)^z;
+  word2=((x&y)|(y&z)|(z&x))<<1;
+  /**************************************************************
+     Don't change anything below here
+  ***************************************************************/
+  return sum(word1,word2);
+}
